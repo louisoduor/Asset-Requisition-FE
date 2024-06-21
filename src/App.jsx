d00efca73d711dcfa1bc35d1f68cc5b69de3f562
louisoduor <louisodhiambo20@gmail.com>
2024-06-21 08:08:15 +0000

    fix: Added the pages and created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AuthWrapper from './AuthWrapper';
+import routes from './routes';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router       
+      basename={import.meta.env.DEV ? '/' : '/ars/'}
+    >
+      <Routes>
+        <Route path="/signin" element={<Login />} /> 
+        <Route path="/" element={<Navigate to="/signin" />} />
+        {routes.map((route, index) => (
+          route.path !== "/signin" &&
+          <Route key={index} path={route.path} element={
+            <AuthWrapper role={route.role} Sidebar={route.Sidebar}>
+              <route.Element />
+            </AuthWrapper>
+          }/>
+        ))}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,13 @@
+
+const routes = [
+    {
+        path: "/signin",
+        Element: Login,
+        isAuthenticated: false,
+        layout: "None",
+        role: null,
+        Sidebar: null,
+    }
+];
+
+export default routes;
