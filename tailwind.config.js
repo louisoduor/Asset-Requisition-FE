b21293dac3bddc7273a933d2e344c05a817f9e42
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-06-21 12:40:39 +0300

    feat: customized colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,25 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode:"class",
   theme: {
     extend: {
-      boxShadow: {
-       
-      },
       colors: {
-        customColor: '#3fa3a6', // Add your custom color
+        neutral:{
+          100:"#FFF",
+          200:"#F7f8FA", 
+          400:"##E0E1E5",          
+          500:"#3d4043",
+          600:"#2d3037",
+          700:"#1c1c1c",         
+          900:"#000"
+        },
+        accent:{          
+          500:"#3FA3A6"
+        },
       },
-      fontFamily:{
-        'body': ['Poppins', 'sans-serif']
-      }
     },
   },
   plugins: [],
