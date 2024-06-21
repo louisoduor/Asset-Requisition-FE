f997517b468d2a603506f134d8563798f4eb0670
louisoduor <louisodhiambo20@gmail.com>
2024-06-21 13:40:03 +0000

    fix : deleted branch auth

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,6 @@
 
 const routes = [
-    {
-        path: "/signin",
-        Element: Login,
-        isAuthenticated: false,
-        layout: "None",
-        role: null,
-        Sidebar: null,
-    }
+
 ];
 
 export default routes;
